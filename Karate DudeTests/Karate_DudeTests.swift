ce6e4768859756315ee9be2d0d99eb481004a9d6
Josh Cenina <jcenina2022@208-engineering-9.local>
2019-04-11 13:28:40 -0700

    Initial Commit

---
+++ Karate DudeTests/Karate_DudeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Karate_DudeTests.swift
+//  Karate DudeTests
+//
+//  Created by Josh Cenina on 4/11/19.
+//  Copyright © 2019 Josh Cenina. All rights reserved.
+//
+
+import XCTest
+@testable import Karate_Dude
+
+class Karate_DudeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
